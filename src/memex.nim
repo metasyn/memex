c5d6dd7ce9ca7f2aa2b7de55282c0d82bef7e286
Xander Johnson <xander@metasyn.pw>
2020-09-28 23:23:39 -0700

    mostly working.

--- src/memex.nim
+++ src/memex.nim
@@ -2,14 +2,13 @@ import os
 import re
 import terminal
 import strutils
-import sequtils
-import sugar
 import strformat
 import tables
 
 import cligen
 import markdown
 
+let linkRegularExpression = re"\[\[(.+?)\]\]"
 
 proc hey(m: string): void =
   styledWriteLine(stdout, fgCyan, "> " & m, resetStyle)
@@ -25,25 +24,20 @@ proc clean(inputDir: string, outputDir: string): void =
   removeDir(outputDir)
   createDir(outputDir.joinPath(inputDir))
 
-proc convertMarkdown(inputDir: string): string =
-  result = markdown(readFile(inputDir))
+proc convertMarkdownToHtml(inputDir: string): string =
+  result = readFile(inputDir)
+    # Replace memex links with markdown ones
+    .replacef(linkRegularExpression, "[$1]($1.html)")
+    .markdown
 
-proc makeBackReferences(items: seq[string]): string =
-  for item in items:
-    result = result & fmt"<a href='{item}.html'>{item}</a>" & " "
+proc makeIncomingLinks(items: seq[string]): string =
+  if items.len > 0:
+    result = "linked from: "
+    for item in items:
+      result = result & " " & fmt"<a href='{item}.html'>{item}</a>"
 
-proc convertFiles(inputDir: string, outputDir: string,
-    templatePath: string): void =
-  hey("Building html files...")
-  if not fileExists(templatePath):
-    nope("templatePath doesn't exist: " & templatePath)
-
-  # For keeping track of back references
-  let
-    linkRegularExpression = re"\[\[(.+?)\]\]"
-    templateContents = readFile(templatePath)
-  var references = newTable[string, seq[string]]()
-
+proc calculateIncomingLinks(inputDir: string): TableRef[string, seq[string]] =
+  result = newTable[string, seq[string]]()
   # First pass for backlinks
   for itemPath in walkpattern(inputDir.joinPath("*.md")):
     if fileExists(itemPath):
@@ -56,23 +50,33 @@ proc convertFiles(inputDir: string, outputDir: string,
         # Nim has some weird content here, not really
         # giving you the subgroups as I'd expect.
         let clean = outlink.replace(re"[\[\]]", "")
-        if references.hasKey(clean):
-          references[clean].add(id)
+        if result.hasKey(clean):
+          result[clean].add(id)
         else:
-          references[clean] = @[id]
+          result[clean] = @[id]
 
-  echo references
+proc convertFiles(inputDir: string, outputDir: string,
+    templatePath: string): void =
+  hey("Building html files...")
+  if not fileExists(templatePath):
+    nope("templatePath doesn't exist: " & templatePath)
+
+  # For keeping track of back references
+  let
+    templateContents = readFile(templatePath)
+    references = calculateIncomingLinks(inputDir)
+
   # Second pass fore templetizing
   for itemPath in walkpattern(inputDir.joinPath("*.md")):
     if fileExists(itemPath):
 
-      let item = itemPath.extractFileName.split(".")[0]
-      let backReferences = references.getOrDefault(item)
+      let
+        item = itemPath.extractFileName.split(".")[0]
+        backReferences = references.getOrDefault(item)
 
       let templetized = templateContents
-        .replace("{{ references }}", makeBackReferences(backreferences))
-        .replacef(linkRegularExpression, "[$1]($1.html)")
-        .replace("{{ content }}", convertMarkdown(itemPath))
+        .replace("{{ content }}", convertMarkdownToHtml(itemPath))
+        .replace("{{ references }}", makeIncomingLinks(backreferences))
 
       let outFile = outputDir
         .joinPath(item)
@@ -99,7 +103,6 @@ proc build(
   convertFiles(inputDir, outputDir, templatePath)
   copyResources(resourcesDir, outputDir)
   hey("Done!")
-
 
 when isMainModule:
   dispatchMulti([build])
