390dfb3a7c07d97b7e4c85662f77275d3a3f4e2d
Xander Johnson <xander@metasyn.pw>
2020-09-30 00:48:54 -0700

    Add timestampting, automatic index generation.

--- src/memex.nim
+++ src/memex.nim
@@ -1,9 +1,12 @@
 import os
 import re
 import terminal
+import sequtils
+import sugar
 import strutils
 import strformat
 import tables
+import times
 
 import cligen
 import markdown
@@ -24,7 +27,20 @@ proc clean(inputDir: string, outputDir: string): void =
   removeDir(outputDir)
   createDir(outputDir.joinPath(inputDir))
 
-proc convertMarkdownToHtml(inputDir: string): string =
+iterator allFilePaths(inputDir: string, ext = ".md"):
+  tuple[path: string, item: string] =
+  for path in walkDirRec(inputDir):
+    let (_, item, extension) = path.splitFile
+    if extension == ext:
+      yield (path, item)
+
+proc convertMarkdownToHtml(input: string): string =
+  # Replace memex links with markdown ones
+  result = input
+    .replacef(linkRegularExpression, "[$1]($1.html)")
+    .markdown
+
+proc convertMarkdownFileToHtml(inputDir: string): string =
   result = readFile(inputDir)
     # Replace memex links with markdown ones
     .replacef(linkRegularExpression, "[$1]($1.html)")
@@ -39,21 +55,91 @@ proc makeIncomingLinks(items: seq[string]): string =
 proc calculateIncomingLinks(inputDir: string): TableRef[string, seq[string]] =
   result = newTable[string, seq[string]]()
   # First pass for backlinks
-  for itemPath in walkpattern(inputDir.joinPath("*.md")):
-    if fileExists(itemPath):
-
-      let id = itemPath.extractFileName.split(".")[0]
-      let content = readFile(itemPath)
-      let links = findall(content, linkRegularExpression)
+  for path, item in allFilePaths(inputDir):
+    if fileExists(path):
+      let
+        content = readFile(path)
+        links = findall(content, linkRegularExpression)
 
       for outlink in links:
         # Nim has some weird content here, not really
         # giving you the subgroups as I'd expect.
         let clean = outlink.replace(re"[\[\]]", "")
         if result.hasKey(clean):
-          result[clean].add(id)
+          result[clean].add(item)
         else:
-          result[clean] = @[id]
+          result[clean] = @[item]
+
+
+proc calculateIndex(inputDir: string): string =
+  var paths = newSeq[seq[string]]()
+  # Calculate hierarchy via file paths
+  for path, item in allFilePaths(inputDir):
+    let parts = path
+      .changeFileExt("")
+      .replace(inputDir, "")
+      .split(DirSep)
+      .filter((x) => x != "")
+    paths.add(parts)
+
+  type
+    Item = ref object
+      name: string
+      terminal: bool
+      children: seq[Item]
+
+  proc `$`(i: Item): string =
+    echo i.name
+    echo i.children.len
+
+  var base = Item(name: "base")
+  var root: Item
+  var sections = newSeq[string]()
+
+  for path in paths:
+    for idx, section in path.pairs:
+      if idx == 0:
+        root = base
+      var child = Item(name: section)
+
+      if not sections.contains(section):
+        sections.add(section)
+
+      if idx == path.len - 1:
+        child.terminal = true
+
+      if not root.children.contains(child):
+        root.children.add(child)
+      root = child
+
+  proc indent(item: Item, depth: int): string =
+    var link = item.name
+    if item.terminal:
+      link = "[[" & link & "]]"
+
+    result = " ".repeat(depth * 2) & fmt"* {link}" & "\n"
+
+  proc recurse(item: Item, depth: int = -1): string =
+    if depth >= 0:
+      # its kinda gross to check for this other sections
+      # list, but its a simple way to keep track fo what
+      # we've already seen for when there are more than
+      # one entry in a folder
+      if sections.contains(item.name):
+        sections.delete(sections.find(item.name))
+        result = result & indent(item, depth)
+
+    # check if we're at the bottom
+    if item.children.len > 0:
+      for child in item.children:
+        result = result & recurse(child, depth + 1)
+    return result
+
+  result = recurse(base)
+
+proc getModificationTime(file: string): string =
+  let time = file.getLastModificationTime.utc.format("YYYY-MM-dd")
+  result = "last edited: " & time.replace("-", ".")
 
 proc convertFiles(inputDir: string, outputDir: string,
     templatePath: string): void =
@@ -63,31 +149,30 @@ proc convertFiles(inputDir: string, outputDir: string,
 
   # For keeping track of back references
   let
+    indexMarkdown = calculateIndex(inputDir)
     templateContents = readFile(templatePath)
-    .replace("{{ index }}", convertMarkdownToHtml(
-        inputDir.joinPath("index.md")))
+      .replace("{{ index }}", convertMarkdownToHtml(indexMarkdown))
   let
     references = calculateIncomingLinks(inputDir)
 
   # Second pass fore templetizing
-  for itemPath in walkpattern(inputDir.joinPath("*.md")):
-    if fileExists(itemPath):
+  for path, item in allFilePaths(inputDir):
+    if fileExists(path):
 
-      let
-        item = itemPath.extractFileName.split(".")[0]
-        backReferences = references.getOrDefault(item)
+      let backReferences = references.getOrDefault(item)
 
       var templetized = templateContents
-        .replace("{{ content }}", convertMarkdownToHtml(itemPath))
+        .replace("{{ content }}", convertMarkdownFileToHtml(path))
         .replace("{{ references }}", makeIncomingLinks(backreferences))
+        .replace("{{ timestamp }}", getModificationTime(path))
 
-      if item == "index":
+      if item == "directory":
         templetized = readFile(templatePath)
-          .replace("{{ content }}", convertMarkdownToHtml(itemPath))
+          .replace("{{ content }}", convertMarkdownToHtml(indexMarkdown))
           .replace("{{ references }}", "")
           .replace("{{ index }}", "")
+          .replace("{{ timestamp }}", getModificationTime(path))
           .replace("details", "div")
-
 
       let outFile = outputDir
         .joinPath(item)
@@ -111,6 +196,8 @@ proc build(
   ): void =
 
   clean(inputDir, outputDir)
+  let index = calculateIndex(inputDir)
+  echo index
   convertFiles(inputDir, outputDir, templatePath)
   copyResources(resourcesDir, outputDir)
   hey("Done!")
