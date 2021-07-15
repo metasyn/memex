2c16b5f99b2cfbe85aa92155724e50f47f30f400
Xander Johnson <xander@metasyn.pw>
2021-07-14 17:48:31 -0700

    got references baseline figured out.

---
+++ content/entries/misc/meta.md
@@ -0,0 +1,24 @@
+# meta
+
+this page is for things related to the making of the memex itself.
+
+# h1
+
+## h2
+
+### h3
+
+> pretty great block quote
+
+code
+```
+internal link: [[memex]]
+extern link: [turtle.audio](https://turtle.audio)
+```
+
+internal link: [[memex]]
+extern link: [turtle.audio](https://turtle.audio)
+
+# todo
+
+custom image
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,19 @@ extern crate colored;
 use colored::*;
 
 use std::collections::HashSet;
+use std::ffi::OsString;
 use std::fmt::Display;
 use std::fs::{self, File};
 use std::io::{BufReader, Read, Result};
 use std::path::{Path, PathBuf};
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+
+///////////
+// UTILS //
+///////////
 
 fn info<T>(msg: T)
 where
@@ -24,9 +33,60 @@ where
     println!("{} {}", "[ERROR]".bright_red().bold(), msg)
 }
 
+// TODO: implement clean
+
+/////////////
+// STRUCTS //
+/////////////
+
+struct Entry {
+    id: OsString,
+    content: String,
+    references: Vec<String>,
+}
+
+////////////
+// REGEX //
+///////////
+
+lazy_static! {static ref INTERNAL_LINK_REGEX: Regex = Regex::new("\\[\\[(.+?)]]").unwrap();}
+
+
+/////////////
+// EXTRACT //
+/////////////
+
+fn extract_content_from_path(path_buf: &PathBuf) -> Option<(OsString, String)> {
+    // check file stem
+    let stem = path_buf.file_stem();
+    if stem.is_none() {
+        err(format!("{:?} has no file stem...", path_buf));
+        return None;
+
+    }
+
+    // check loading content
+    let content = load_file(path_buf);
+    if content.is_err() {
+        err(format!("{:?} content could not be loaded...", path_buf));
+        return None
+    }
+    return Some((OsString::from(stem.unwrap()), content.unwrap()));
+
+}
+
+fn extract_references_from_content(body: &str) -> Vec<String> {
+    INTERNAL_LINK_REGEX
+        .captures_iter(body)
+        .filter_map(|c| c.get(1))
+        .map(|x| String::from(x.as_str()))
+        .collect()
+}
+
 // recursively find all .md files in the content path
-fn collect_entries(content_path: &str) -> Result<Vec<PathBuf>> {
-    let mut result: Vec<PathBuf> = Vec::new();
+// and turn them into entries
+fn collect_entries(content_path: &str) -> Result<Vec<Entry>> {
+    let mut result: Vec<Entry> = Vec::new();
     let mut todo: Vec<PathBuf> = Vec::new();
     let root = Path::new(content_path).to_path_buf();
     todo.push(root);
@@ -45,10 +105,20 @@ fn collect_entries(content_path: &str) -> Result<Vec<PathBuf>> {
             .map(|e| e.to_path_buf())
             .collect::<HashSet<PathBuf>>();
 
-        let files: Vec<_> = entries
+        let files: Vec<Entry> = entries
             .difference(&directories)
-            .filter(|e| e.extension().unwrap() == "md")
-            .map(|e| e.to_path_buf())
+            .filter(|e| match e.extension() {
+                Some(ext) => ext == "md",
+                None => false
+            })
+            .filter_map(|e| extract_content_from_path(e))
+            .map(|(stem, content)|
+                Entry{
+                    id: OsString::from(stem),
+                    content: content.clone(),
+                    references: extract_references_from_content(content.as_str()),
+                }
+            )
             .collect();
 
         todo.extend(directories.into_iter().collect::<Vec<PathBuf>>());
@@ -57,32 +127,33 @@ fn collect_entries(content_path: &str) -> Result<Vec<PathBuf>> {
     return Ok(result);
 }
 
-// TODO: implement clean
+//////////////
+// COMMANDS //
+//////////////
 
 // TODO: pass template path
 fn build(content_path: &str) -> Result<()> {
     info("building memex...");
 
     match collect_entries(content_path) {
-        Ok(paths) => {
+        Ok(entries) => {
             info(
                 format!(
                     "compiling {} entries...",
-                    paths.len().to_string().bright_yellow()
+                    entries.len().to_string().bright_yellow()
                 )
-                .as_str(),
             );
             // handle specific files here
             let base_template = load_file("./templates/base.html")?;
-            //
-            for path in paths {
+
+            for entry in entries{
                 // TODO: keep track of references before doing replacements
                 // TODO: also calculate directory
                 // finish replacements
                 // write files
-                let contents = load_file(path)?;
-
-                let replacements = vec![("directory", "something"), ("content", contents.as_str())];
+                let contents = entry.content.as_str();
+                let references = format_references(entry.references);
+                let replacements = vec![("content", contents), ("references", &references)];
                 let replaced = replace_templates(base_template.clone(), replacements);
                 info(replaced);
             }
@@ -92,6 +163,7 @@ fn build(content_path: &str) -> Result<()> {
     Ok(())
 }
 
+
 fn load_file<P: AsRef<Path>>(path: P) -> std::io::Result<String> {
     let file = File::open(path)?;
     let mut buf_reader = BufReader::new(file);
@@ -99,6 +171,18 @@ fn load_file<P: AsRef<Path>>(path: P) -> std::io::Result<String> {
     buf_reader.read_to_string(&mut contents)?;
     return Ok(contents);
 }
+
+fn format_references(references: Vec<String>) -> String {
+    references
+        .iter()
+        .map(|x| format!("[[{}]]", x))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+///////////////
+// TEMPLATES //
+///////////////
 
 fn make_template(item: &str) -> String {
     return format!("{{{{ {} }}}}", item);
@@ -114,6 +198,10 @@ fn replace_templates<'a>(mut body: String, mapping: Vec<(&str, &str)>) -> String
     }
     return body;
 }
+
+//////////
+// MAIN //
+//////////
 
 fn main() -> Result<()> {
     let matches = App::new("memex")
@@ -142,6 +230,10 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+///////////
+// TESTS //
+///////////
+
 #[cfg(test)]
 mod tests {
     // import namespace above here
@@ -169,5 +261,23 @@ mod tests {
             ),
             "fab replacement"
         )
+    }
+
+    #[test]
+    fn test_extract_content_from_path() {
+        let path_buf = PathBuf::from("Cargo.toml");
+        let res = extract_content_from_path(&path_buf);
+        assert!(res.is_some());
+        assert!(res.unwrap().0 == "Cargo");
+    }
+
+
+    #[test]
+    fn test_extract_references_from_content() {
+        let res = extract_references_from_content("testing [[one]] [[two]]");
+        assert_eq!(res.len(), 2);
+        assert_eq!(res[0], "one");
+        assert_eq!(res[1], "two");
+
     }
 }
