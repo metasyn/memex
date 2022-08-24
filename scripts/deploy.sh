ce86c6899765603e491f740871d34136565f59e1
xander johnson <xander@metasyn.pw>
2022-08-23 22:12:52 -0700

    update script for termux maybe?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 sshopts="ssh -o StrictHostKeyChecking=no -p 23"
-rsync --rsh="$sshopts" -zavhrc ./dist/* xander@metasyn.pw:/var/www/nginx/memex
+rsync --rsh="$sshopts" --chmod=755 -zavhrc ./dist/* xander@metasyn.pw:/var/www/nginx/memex
